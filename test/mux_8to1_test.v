79f3ab23a40f83cc2da322fee90120c1cd82b6fc
ehab93 <ehalbadawy93@gmail.com>
2014-11-24 23:41:51 +0200

    modify test to fit with last mux modification

--- test/mux_8to1_test.v
+++ test/mux_8to1_test.v
@@ -2,14 +2,12 @@
 
 module test;
 
-  reg s2, s1, s0;
+  reg [2:0] s;
   reg i7, i6, i5, i4, i3, i2, i1, i0;
 
   wire z;
   mux_8to1 mux(
-    .s2(s2),
-    .s1(s1),
-    .s0(s0),
+    .s (s ),
     .i7(i7),
     .i6(i6),
     .i5(i5),
@@ -30,14 +28,14 @@ module test;
 // 111 7
  
   initial begin
-    $monitor($time, ": %b  %b  %b  %b  %b  %b  %b  %b | %b  %b  %b | %b", i7,  i6,  i5,  i4,  i3,  i2,  i1,  i0,  s2,  s1,  s0, z);
+    $monitor($time, ": %b  %b  %b  %b  %b  %b  %b  %b | %b  %b  %b | %b", i7,  i6,  i5,  i4,  i3,  i2,  i1,  i0,  s[2],  s[1],  s[0], z);
     
-    #5 i7 = 0; i6 = 0; i5 = 0;  i4 = 0; i3 = 0; i2 = 0; i1 = 0; i0 = 0; s2 = 0; s1 = 0; s0 = 0;
-    #5 i4 = 1; i5 = 1; i2 = 1; s1 = 1; // 010 select i2
-    #5 s0 = 1; // 011 select i3
-    #5 s2 = 1; // 111 select i7
+    #5 i7 = 0; i6 = 0; i5 = 0;  i4 = 0; i3 = 0; i2 = 0; i1 = 0; i0 = 0; s[2] = 0; s[1] = 0; s[0] = 0;
+    #5 i4 = 1; i5 = 1; i2 = 1; s[1] = 1; // 010 select i2
+    #5 s[0] = 1; // 011 select i3
+    #5 s[2] = 1; // 111 select i7
     #5 i7 = 0;
-    #5 s1 = 0; // 101 select i5
+    #5 s[1] = 0; // 101 select i5
     #5 i5 = 1;
   end
 
