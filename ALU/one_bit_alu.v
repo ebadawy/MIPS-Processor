393ae2a382562481e4fb5cd1e3e0242b14ff98c5
ehab93 <ehalbadawy93@gmail.com>
2014-11-26 14:04:02 +0200

    Create one bit alu

--- ALU/one_bit_alu.v
+++ ALU/one_bit_alu.v
@@ -14,19 +14,18 @@ or  (or_op, a, b);
 xor (xor_op, a, b);
 nor (xnor_op, a, b);
 
-
 full_adder add     (.sum(and_op), .a(a), .b(b    ), .cin(cin), .cout(cout));
 full_adder subtract(.sum(sub_op), .a(a), .b(xor_b), .cin(cin), .cout(cout));
 
 or (set, sub_op, 1'b1);
-
+wire rr;
 mux_16to1 mux(
   .s  (op    ),
   .i0 (and_op),
   .i1 (or_op ),
   .i2 (add_op),
   .i6 (sub_op),
-  .i7 (slt_op),
+  .i7 (less  ),
   .i12(nor_op),
   .i3 (1'b0  ),
   .i4 (1'b0  ),
@@ -37,6 +36,7 @@ mux_16to1 mux(
   .i11(1'b0  ),
   .i13(1'b0  ),
   .i14(1'b0  ),
-  .i15(1'b0 ));
+  .i15(1'b0  ),
+  .z  (r    ));
 
 endmodule
