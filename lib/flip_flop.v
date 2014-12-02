ebd1b45e0e9f43429e5f208ad42225e93467a968
ehab93 <ehalbadawy93@gmail.com>
2014-12-02 06:37:10 +0200

    Add write enable

--- lib/flip_flop.v
+++ lib/flip_flop.v
@@ -1,11 +1,19 @@
+`include "../lib/mux_2to1.v"
+
 module d_flip_flop (
-  input d, clk,
+  input d, clk, we,
   output q, q_bar);
 
-wire int0, int1;
+wire int0, int1, ff_input;
 
-nand (int0, d   , clk);
-nand (int1, int0, clk);
+mux_2to1 mux(
+    .z(ff_input),
+    .i0(q),
+    .i1(d),
+    .s(we));
+
+nand (int0, ff_input, clk);
+nand (int1, int0    , clk);
 
 nand (q    , int0, q_bar);
 nand (q_bar, int1, q    );
