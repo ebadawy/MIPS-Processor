6e41e16b8d40866860c787c3c55fc57663f8cbc3
ehab93 <ehalbadawy93@gmail.com>
2014-12-10 05:46:30 +0200

    fix code style

--- ALU/one_bit_alu.v
+++ ALU/one_bit_alu.v
@@ -4,32 +4,31 @@
 
 module one_bit_alu (
   input  [3:0] op,
-  input  a, b, cin, less, sub,
+  input  a, b, cin, less,
   output r, cout, bout, set
   );
  
-//wire andOP, int1, xorOP, norOP, addOP, sltOP, xorB;
 wire int0, int1, int2, int3, addOP, subOP, addOP_cout, subOP_bout;
-and (int0 , a, b);
-or  (int1  , a, b);
-xor (int2 , a, b);
+and (int0, a, b);
+or  (int1, a, b);
+xor (int2, a, b);
 nor (int3, a, b);
 
 xor (xorB, b, sub);
 
-full_adder       add     (.sum(addOP), .a(a), .b(b), .cin(cin), .cout(addOP_cout));
+full_adder       add     (.sum(addOP), .a(a), .b(b), .cin   (cin), .cout   (addOP_cout));
 full_subtractor  subtract(.d  (subOP), .a(b), .b(a), .Bor_in(cin), .Bor_out(subOP_bout));
 
 buf(set, subOP);
 
 mux_16to1 mux0(
   .s  (op    ),
-  .i0 (int0  ),
-  .i1 (int1 ),
-  .i2 (addOP),
-  .i6 (subOP),
-  .i7 (less  ),
-  .i12(int3),
+  .i0 (int0  ), // and
+  .i1 (int1  ), // or
+  .i2 (addOP ), // add
+  .i6 (subOP ), // sub
+  .i7 (less  ), // less
+  .i12(int3  ), // nor
   .z  (r    ));
 
 mux_16to1 mux1(
