e994b208daf6be9958e17794bcc654ce533f1f69
ehab93 <ehalbadawy93@gmail.com>
2014-12-22 10:50:28 +0200

    Modify test fiels

--- test/cpu_test.v
+++ test/cpu_test.v
@@ -5,49 +5,64 @@ module cpu_test;
   reg clk;
   reg [31:0] addr;
   wire [31:0] alu_output, data;
-  cpu c(.clk(clk), .alu_output(alu_output), .data(data));
-
+  wire [31:0] pc_4;
+  cpu c(.clk(clk), .alu_output(alu_output), .data(data), .nxt_pc(pc_4));
+  reg [31:0] regVal;
   integer i;
   always #1 clk = ~clk;
   initial begin
     $dumpfile("cpu.vcd");
     $dumpvars(0, cpu_test);
     clk = 0;
-    for(i = 0; i < 223; i = i + 1) begin
-     #2; //$display($time, ": data = %h, alu_output = %h", data, alu_output); 
+   // for(i = 0; i < 223; i = i + 1) begin
+   // #2; $display("pc_4 = %h, data = %h", pc_4, data);
+     // if($isunknown(data)) begin 
+      //  break;
+     // end
+    //end
+    i = 0;
+    while (i < 233) begin
+    #2 ;//$display("pc_4 = %h, data = %b", pc_4, data);
+    i = i + 1;
     end
-    #2; $display("data = %h, $0  = %h", data, alu_output);
-    #2; $display("data = %h, $at = %h", data, alu_output);
-    #2; $display("data = %h, $v0 = %h", data, alu_output);
-    #2; $display("data = %h, $v1 = %h", data, alu_output);
-    #2; $display("data = %h, $a0 = %h", data, alu_output);
-    #2; $display("data = %h, $a1 = %h", data, alu_output);
-    #2; $display("data = %h, $a2 = %h", data, alu_output);
-    #2; $display("data = %h, $a3 = %h", data, alu_output);
-    #2; $display("data = %h, $t0 = %h", data, alu_output);
-    #2; $display("data = %h, $t1 = %h", data, alu_output);
-    #2; $display("data = %h, $t2 = %h", data, alu_output);
-    #2; $display("data = %h, $t3 = %h", data, alu_output);
-    #2; $display("data = %h, $t4 = %h", data, alu_output);
-    #2; $display("data = %h, $t5 = %h", data, alu_output);
-    #2; $display("data = %h, $t6 = %h", data, alu_output);
-    #2; $display("data = %h, $t7 = %h", data, alu_output);
-    #2; $display("data = %h, $s0 = %h", data, alu_output);
-    #2; $display("data = %h, $s1 = %h", data, alu_output);
-    #2; $display("data = %h, $s2 = %h", data, alu_output);
-    #2; $display("data = %h, $s3 = %h", data, alu_output);
-    #2; $display("data = %h, $s4 = %h", data, alu_output);
-    #2; $display("data = %h, $s5 = %h", data, alu_output);
-    #2; $display("data = %h, $s6 = %h", data, alu_output);
-    #2; $display("data = %h, $s7 = %h", data, alu_output);
-    #2; $display("data = %h, $t8 = %h", data, alu_output);
-    #2; $display("data = %h, $t9 = %h", data, alu_output);
-    #2; $display("data = %h, $k0 = %h", data, alu_output);
-    #2; $display("data = %h, $k1 = %h", data, alu_output);
-    #2; $display("data = %h, $gp = %h", data, alu_output);
-    #2; $display("data = %h, $sp = %h", data, alu_output);
-    #2; $display("data = %h, $fp = %h", data, alu_output);
-    #2; $display("data = %h, $ra = %h", data, alu_output);
-    $finish;
+    
+    //for(i = 0; i < 32; i = i + 1) begin
+     // regVal = c.rf.regs[i];
+     // #2 $display("pc_4 = %h, data = %h, $s%d  = %h", pc_4, data, i,regVal);
+    //end
+    //#2; $display("Final value of PC = %h", pc_4 );
+    #2; $display("$0  = %h", c.rf.regs[0 ] );
+    #2; $display("$at = %h", c.rf.regs[1 ] );
+    #2; $display("$v0 = %h", c.rf.regs[2 ] );
+    #2; $display("$v1 = %h", c.rf.regs[3 ] );
+    #2; $display("$a0 = %h", c.rf.regs[4 ] );
+    #2; $display("$a1 = %h", c.rf.regs[5 ] );
+    #2; $display("$a2 = %h", c.rf.regs[6 ] );
+    #2; $display("$a3 = %h", c.rf.regs[7 ] );
+    #2; $display("$t0 = %h", c.rf.regs[8 ] );
+    #2; $display("$t1 = %h", c.rf.regs[9 ] );
+    #2; $display("$t2 = %h", c.rf.regs[10] );
+    #2; $display("$t3 = %h", c.rf.regs[11] );
+    #2; $display("$t4 = %h", c.rf.regs[12] );
+    #2; $display("$t5 = %h", c.rf.regs[13] );
+    #2; $display("$t6 = %h", c.rf.regs[14] );
+    #2; $display("$t7 = %h", c.rf.regs[15] );
+    #2; $display("$s0 = %h", c.rf.regs[16] );
+    #2; $display("$s1 = %h", c.rf.regs[17] );
+    #2; $display("$s2 = %h", c.rf.regs[18] );
+    #2; $display("$s3 = %h", c.rf.regs[19] );
+    #2; $display("$s4 = %h", c.rf.regs[20] );
+    #2; $display("$s5 = %h", c.rf.regs[21] );
+    #2; $display("$s6 = %h", c.rf.regs[22] );
+    #2; $display("$s7 = %h", c.rf.regs[23] );
+    #2; $display("$t8 = %h", c.rf.regs[24] );
+    #2; $display("$t9 = %h", c.rf.regs[25] );
+    #2; $display("$k0 = %h", c.rf.regs[26] );
+    #2; $display("$k1 = %h", c.rf.regs[27] );
+    #2; $display("$gp = %h", c.rf.regs[28] );
+    #2; $display("$sp = %h", c.rf.regs[29] );
+    #2; $display("$fp = %h", c.rf.regs[30] );
+    #2; $display("$ra = %h", c.rf.regs[31] );
+    $finish;                                      
   end
 endmodule
